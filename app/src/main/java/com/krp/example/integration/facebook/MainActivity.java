e6e6c1bf4538e65989cb5363f16c8f1b97d6f5f1
kumarpurushottam <krp.pandit@gmail.com>
2015-12-22 00:35:20 +0530

    Addition: Facebook integration with Android

--- app/src/main/java/com/krp/example/integration/facebook/MainActivity.java
+++ app/src/main/java/com/krp/example/integration/facebook/MainActivity.java
@@ -3,11 +3,14 @@ package com.krp.example.integration.facebook;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.FacebookSdk;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
     }
 }
